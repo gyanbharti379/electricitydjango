9a5547c7432583ac8bcdad42ae9a4446f7998199
Gyan Bharti <166585279+gyanbharti379@users.noreply.github.com>
2024-07-06 18:30:21 +0530

    Delete customer directory

--- customer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer,MeterInfo,ServiceCharge,tax,bill
-
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(MeterInfo)
-admin.site.register(ServiceCharge)
-admin.site.register(tax)
-admin.site.register(bill)
